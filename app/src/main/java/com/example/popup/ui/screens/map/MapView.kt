3e4200d7a2d2f8faa028429580d20ba52758c685
Benjamin Michael <ben29michael@gmail.com>
2024-10-11 19:53:52 -0700

    added map view, and model

---
+++ app/src/main/java/com/example/popup/ui/screens/map/MapView.kt
@@ -0,0 +1,97 @@
+package com.example.popup.ui.screens.map
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.popup.di.location.LocationHandler
+import com.example.popup.mock.MockApiService
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
+
+/**
+ * The map screen view
+ *
+ * @author Benjamin Michael
+ * Project: Pop-Up
+ * Created on: 10/11/2024
+ */
+@Composable
+fun MapView(
+    viewModel: MapViewModel = hiltViewModel()
+) {
+    val userLocation by viewModel.userLocation.collectAsState()
+    val markers by viewModel.postMarkers.collectAsState()
+
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(userLocation, 15f)
+    }
+
+    LaunchedEffect(userLocation) {
+        cameraPositionState.animate(
+            update = CameraUpdateFactory.newLatLngZoom(userLocation, 15f),
+            durationMs = 1000
+        )
+    }
+
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        GoogleMap(
+            modifier = Modifier.fillMaxSize(),
+            properties = MapProperties(
+                isMyLocationEnabled = true
+            ),
+            cameraPositionState = cameraPositionState,
+            onMapLoaded = {
+                val initialPosition = cameraPositionState.position.target
+                val currentBounds = LatLngBounds.Builder()
+                    .include(initialPosition)
+                    .build()
+
+                viewModel.onEvent(MapViewEvent.MapBoundsChanged(currentBounds))
+            }
+        ) {
+            markers.forEach { marker ->
+                Marker(
+                    state = MarkerState(
+                        position = LatLng(marker.location.latitude, marker.location.longitude)
+                    ),
+                    onClick = {
+                        viewModel.onEvent(
+                            event = MapViewEvent.PostClicked(
+                                id = marker.id
+                            )
+                        )
+                        true
+                    }
+                    // TODO("Set custom marker depending on marker.postType")
+                )
+            }
+        }
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun MapViewPreview(
+
+) {
+    MapView(
+        viewModel = MapViewModel(
+            apiService = MockApiService(),
+            locationHandler = LocationHandler()
+        )
+    )
+}
---
+++ app/src/main/java/com/example/popup/ui/screens/map/MapViewModel.kt
@@ -0,0 +1,114 @@
+package com.example.popup.ui.screens.map
+
+import android.location.Location as AndroidLocation
+import androidx.lifecycle.viewModelScope
+import com.example.popup.di.location.ILocationUpdatedListener
+import com.example.popup.di.location.LocationHandler
+import com.example.popup.model.domain.common.Location
+import com.example.popup.model.request.post.GetMapDataRequest
+import com.example.popup.model.response.PostMapData
+import com.example.popup.networking.api.IApiService
+import com.example.popup.ui.util.APopUpViewModel
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+/**
+ * The map screen view model
+ *
+ * @author Benjamin Michael
+ * Project: Pop-Up
+ * Created on: 10/11/2024
+ */
+@HiltViewModel
+class MapViewModel @Inject constructor(
+    private val apiService: IApiService,
+    private val locationHandler: LocationHandler
+): APopUpViewModel<MapViewEvent>(), ILocationUpdatedListener {
+
+    /**
+     * Flow that stores the post map data loaded
+     */
+    private val _postMarkers = MutableStateFlow<Set<PostMapData>>(emptySet())
+    val postMarkers: StateFlow<Set<PostMapData>> = _postMarkers.asStateFlow()
+
+    /**
+     * Track the user's location
+     */
+    private val _userLocation = MutableStateFlow(LatLng(0.0, 0.0))
+    val userLocation: StateFlow<LatLng> = _userLocation.asStateFlow()
+
+    /**
+     * Default constructor
+     */
+    init {
+        setupLocationListener()
+    }
+
+    /**
+     * Add this view model as a listener for location updates, and set the user location to the
+     * last know location
+     */
+    private fun setupLocationListener() {
+        locationHandler.addListener(this)
+    }
+
+    /**
+     * Catch when the view model is cleared and remove it as a listener from the location handler
+     */
+    override fun onCleared() {
+        super.onCleared()
+
+        locationHandler.removeListener(this)
+    }
+
+    /**
+     * Handle map view events
+     */
+    override fun onEvent(event: MapViewEvent) {
+        when (event) {
+            is MapViewEvent.MapBoundsChanged -> getPostMapDataForBound(event.bounds)
+            is MapViewEvent.PostClicked -> TODO()
+        }
+    }
+
+    /**
+     * Get post map data for the new bounds
+     */
+    private fun getPostMapDataForBound(bounds: LatLngBounds) {
+        viewModelScope.launch {
+            val northeast = Location(bounds.northeast.latitude, bounds.northeast.longitude)
+            val southwest = Location(bounds.southwest.latitude, bounds.southwest.longitude)
+            val northwest = Location(bounds.northeast.latitude, bounds.southwest.longitude)
+            val southeast = Location(bounds.southwest.latitude, bounds.northeast.longitude)
+
+            val request = GetMapDataRequest(
+                topLeft = northwest,
+                topRight = northeast,
+                bottomRight = southeast,
+                bottomLeft = southwest
+            )
+
+            val result = apiService.getPostMapData(request)
+            if (result.wasSuccessful() && result.data != null) {
+                // Merge the two sets of markers
+                val updatedMarkers = result.data.toSet()
+                updatedMarkers.plus(_postMarkers.value)
+
+                _postMarkers.value = updatedMarkers
+            }
+        }
+    }
+
+    /**
+     * Listen for location updates
+     */
+    override fun locationUpdated(location: AndroidLocation) {
+        _userLocation.value = LatLng(location.latitude, location.longitude)
+    }
+}
