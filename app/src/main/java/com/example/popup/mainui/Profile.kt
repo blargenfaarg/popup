d566fa5ea85340998236cbd0220b0c06df39f507
Benjamin Michael <ben29michael@gmail.com>
2024-10-13 12:26:04 -0700

    remove unused imports

--- app/src/main/java/com/example/popup/mainui/Profile.kt
+++ app/src/main/java/com/example/popup/mainui/Profile.kt
@@ -36,8 +36,6 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun ProfileScreen(
