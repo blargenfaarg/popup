aa647dd90f13827d26579674acfed88fc201e422
Benjamin Michael <ben29michael@gmail.com>
2024-10-13 12:24:15 -0700

    added options for default image, size, etc.

--- app/src/main/java/com/example/popup/ui/reusable/ProfileImage.kt
+++ app/src/main/java/com/example/popup/ui/reusable/ProfileImage.kt
@@ -1,6 +1,5 @@
 package com.example.popup.ui.reusable
 
-import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
@@ -9,8 +8,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.CameraAlt
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -21,7 +18,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.core.net.toFile
 import coil.compose.AsyncImage
@@ -37,15 +37,20 @@ import java.io.File
 @Composable
 fun ProfileImage(
     allowPick: Boolean = true,
-    onPick: (File) -> Unit
+    defaultPicture: ImageVector,
+    onPick: ((File) -> Unit)? = null,
+    image: Any? = null,
+    size: Dp = 120.dp
 ) {
-    var selectedImageUri by remember { mutableStateOf<Uri?>(null) }
+    var selectedImageUri by remember { mutableStateOf(image) }
     val pickMedia = rememberLauncherForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
         uri?.let {
             selectedImageUri = uri
             try {
                 val file = uri.toFile()
-                onPick(file)
+                onPick?.let {
+                    it(file)
+                }
             } catch (e: Exception) {
                 println("Caught exception when converting file -> ${e.message}")
             }
@@ -54,7 +59,7 @@ fun ProfileImage(
 
     Box(
         modifier = Modifier
-            .size(120.dp)
+            .size(size)
             .clip(CircleShape)
             .background(Color.LightGray)
             .clickable {
@@ -74,7 +79,7 @@ fun ProfileImage(
             )
         } else {
             Icon(
-                imageVector = Icons.Filled.CameraAlt,
+                imageVector = defaultPicture,
                 contentDescription = "Profile Picture",
                 modifier = Modifier
                     .size(40.dp)
